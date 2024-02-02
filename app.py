9b41fe678961c31d6fa07bf848e1b93340f94324
Jawad Safi <jsafi4428@gmail.com>
2024-02-02 09:50:28 -0500

    making the app more dynamic

--- app.py
+++ app.py
@@ -4,5 +4,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    return render_template('index.html')
+
+@app.route('/greeting')
+def greeting():
     name = request.args.get('name')
-    return render_template('index.html',name=name)
+    return render_template('greetings.html',name=name)
+ 
