d31ca423271a6760987b22720c59a3eedb510b72
Jawad Safi <jsafi4428@gmail.com>
2024-02-02 11:01:55 -0500

    adding the form to square the number

--- app.py
+++ app.py
@@ -2,12 +2,19 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.get('/')
 def index():
     return render_template('index.html')
 
-@app.route('/greeting', methods=['POST'])
+@app.post('/greeting')
 def greeting():
     py_name = request.form.get('form_name')
     return render_template('greetings.html',template_name=py_name)
+
+@app.post('/square')
+def square():
+    py_num = request.form.get('num',0, type=int)
+    print(py_num)
+    square = py_num * py_num
+    return render_template('square.html',num=py_num, square_num=square)
  
